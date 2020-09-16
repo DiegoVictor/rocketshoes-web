c1e70bed40a03359c1329e54ffff0ec9dbb8ef88
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-16 19:53:02 -0300

    Remove reactotron monitor and middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,11 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import Reactotron from 'reactotron-react-js';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from '~/store/modules/rootReducer';
 import rootSaga from '~/store/modules/rootSaga';
 
-const sagaMiddleware = createSagaMiddleware({
-  sagaMonitor: (() => {
-    if (process.env.NODE_ENV === 'development') {
-      return Reactotron.createSagaMonitor();
-    }
-    return null;
-  })(),
-});
-
-const store = createStore(
-  rootReducer,
-  (() => {
-    if (process.env.NODE_ENV === 'development') {
-      return compose(
-        Reactotron.createEnhancer(),
-        applyMiddleware(sagaMiddleware)
-      );
-    }
-    return applyMiddleware(sagaMiddleware);
-  })()
-);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
