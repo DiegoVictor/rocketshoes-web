b1d8172ad346409b2725e1725a9459e5c625a514
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-17 18:07:28 -0300

    Create main entry point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
+import { Router } from 'react-router-dom';
 
 import Routes from '~/routes';
 import Header from '~/components/Header';
 import history from '~/services/history';
-import '~/config/ReactotronConfig';
+import Theme from '~/styles/theme';
 import store from '~/store';
-import Style from './styles';
 
 export default () => {
   return (
     <Provider store={store}>
+      <Theme />
+      <ToastContainer autoClose={3000} />
+
       <Router history={history}>
         <Header />
-        <Style />
         <Routes />
-        <ToastContainer autoClose={3000} />
       </Router>
     </Provider>
   );
