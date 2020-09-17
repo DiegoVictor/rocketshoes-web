b5ac940a2d6a9ca2f2985dd797de3f89d8947f0a
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-17 18:08:30 -0300

    Fix from eslint

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -8,7 +8,7 @@ import { formatPrice } from '~/util/format';
 
 export function* addToCart({ id }) {
   const productExists = yield select(state =>
-    state.cart.find(p => p.id === id)
+    state.cart.find(p => p.id === id),
   );
 
   const stock = yield call(api.get, `/stock/${id}`);
